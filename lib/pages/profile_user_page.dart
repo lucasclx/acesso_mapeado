aad613c471e3cc93dda4d793a5e4e22a66d98fb5
Jasmine Pinheiro <jasminepinheiro14@gmail.com>
2024-09-02 17:40:06 -0300

    feat: Creating the form to edit the user profile

--- lib/pages/profile_user_page.dart
+++ lib/pages/profile_user_page.dart
@@ -22,13 +22,150 @@ class ProfileUserPage extends StatelessWidget {
           ]),
         ),
         leading: IconButton(
-          icon: Image.asset('assets/icons/arrow.png'),
+          icon: Image.asset('assets/icons/arrow-left.png'),
           onPressed: () {
             Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => const HomePage()));
+              context,
+              MaterialPageRoute(builder: (context) => const HomePage()),
+            );
           },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 30),
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        labelText: 'Nome',
+                        border: OutlineInputBorder(),
+                        contentPadding: EdgeInsets.symmetric(
+                            horizontal: 16.0, vertical: 12.0)),
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        labelText: 'Data de nascimento',
+                        border: OutlineInputBorder(),
+                        contentPadding: EdgeInsets.symmetric(
+                            horizontal: 16.0, vertical: 12.0)),
+                    keyboardType: TextInputType.datetime,
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        labelText: 'CPF',
+                        border: OutlineInputBorder(),
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 16, vertical: 12)),
+                    keyboardType: TextInputType.number,
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        labelText: 'E-mail',
+                        border: OutlineInputBorder(),
+                        contentPadding: EdgeInsets.symmetric(
+                            horizontal: 16.0, vertical: 12.0)),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        labelText: 'Senha',
+                        border: OutlineInputBorder(),
+                        contentPadding: EdgeInsets.symmetric(
+                            horizontal: 16.0, vertical: 12.0)),
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        labelText: 'Confirmar senha',
+                        border: OutlineInputBorder(),
+                        contentPadding: EdgeInsetsDirectional.symmetric(
+                            horizontal: 16.0, vertical: 12.0)),
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 40),
+                  ElevatedButton(
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                            content: Text('Atualizações salvas com sucesso')),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColors.lightPurple,
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 24.0, vertical: 12.0)),
+                    child: const Text(
+                      'Salvar',
+                      style: TextStyle(
+                          color: AppColors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                    ),
+                  ),
+                  const SizedBox(height: 45),
+                  Row(
+                    children: [
+                      const Icon(Icons.lock_clock_outlined,
+                          color: AppColors.lightPurple),
+                      const SizedBox(width: 8),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Redefinir senha',
+                          style: TextStyle(
+                              color: AppColors.black,
+                              fontWeight: FontWeight.normal),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.delete_outline,
+                          color: AppColors.lightPurple),
+                      const SizedBox(width: 8),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Excluir conta',
+                          style: TextStyle(
+                              color: AppColors.black,
+                              fontWeight: FontWeight.normal),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.exit_to_app,
+                          color: AppColors.lightPurple),
+                      const SizedBox(width: 8),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Sair',
+                          style: TextStyle(
+                            color: AppColors.black,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
