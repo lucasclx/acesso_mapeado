7f3eef76e1e9f5f084db80773bbb978f0e922e29
Jasmine Pinheiro <jasminepinheiro14@gmail.com>
2024-09-02 17:44:56 -0300

    feat: Adding a new color variable

--- lib/shared/app_colors.dart
+++ lib/shared/app_colors.dart
@@ -8,4 +8,5 @@ class AppColors {
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
   static const Color darkGray = Color(0xFF8E8E8E);
+  static const Color lightGray = Color(0xFFB7B7B7);
 }
