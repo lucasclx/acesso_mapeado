8ed0941008ba52ac459c9edacb5446c9de539256
Jasmine Pinheiro <jasminepinheiro14@gmail.com>
2024-09-05 21:57:55 -0300

    feat: adding a new purple color variable

--- lib/shared/app_colors.dart
+++ lib/shared/app_colors.dart
@@ -4,7 +4,7 @@ class AppColors {
   AppColors._(); // instanciar propriedade do meu objeto
 
   static const Color lightPurple = Color(0XFFD268CC);
-  static const Color darkPurple = Color(0XFFC94AC2);
+  static const Color veryLightPurple  = Color(0XFFFFEDFE);
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
   static const Color darkGray = Color(0xFF8E8E8E);
