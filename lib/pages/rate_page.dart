c5a15d89259e853b5d0c8fa32359f37fea11f0de
Jasmine Pinheiro <jasminepinheiro14@gmail.com>
2024-09-12 15:07:41 -0300

    feat: removing the unused import 'package:flutter/widgets.dart'

--- lib/pages/rate_page.dart
+++ lib/pages/rate_page.dart
@@ -2,7 +2,6 @@ import 'package:acesso_mapeado/pages/home_page.dart';
 import 'package:acesso_mapeado/shared/app_colors.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class RatePage extends StatefulWidget {
