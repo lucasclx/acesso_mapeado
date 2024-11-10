e4a92f5f453afd10448a04999b0761cfce5e1eaa
Jasmine Pinheiro <jasminepinheiro14@gmail.com>
2024-11-10 19:11:10 -0300

    feat: add scroll with SingleChildScrollView

--- lib/pages/rate_page.dart
+++ lib/pages/rate_page.dart
@@ -126,100 +126,103 @@ class _RatePageState extends State<RatePage> {
         ),
       ),
       backgroundColor: AppColors.white,
-      body: Column(
-        children: [
-          const SizedBox(height: 10),
-          Container(
-            padding: const EdgeInsets.all(10),
-            child: const Row(
-              children: [
-                SizedBox(width: 16),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Nome do Usuário',
-                      style:
-                          TextStyle(fontSize: 18, color: AppColors.lightPurple),
-                    ),
-                    SizedBox(height: 4),
-                    Row(
-                      children: [
-                        Text(
-                          'Sua postagem será pública.',
-                          style: TextStyle(
-                              fontSize: 14, color: AppColors.darkGray),
-                        ),
-                        Icon(Icons.warning_rounded,
-                            color: AppColors.lightPurple),
-                      ],
-                    )
-                  ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: const Row(
+                children: [
+                  SizedBox(width: 16),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Nome do Usuário',
+                        style: TextStyle(
+                            fontSize: 18, color: AppColors.lightPurple),
+                      ),
+                      SizedBox(height: 4),
+                      Row(
+                        children: [
+                          Text(
+                            'Sua postagem será pública.',
+                            style: TextStyle(
+                                fontSize: 14, color: AppColors.darkGray),
+                          ),
+                          Icon(Icons.warning_rounded,
+                              color: AppColors.lightPurple),
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: RatingBar.builder(
+                initialRating: _rating,
+                minRating: 1,
+                direction: Axis.horizontal,
+                allowHalfRating: true,
+                itemCount: 5,
+                itemBuilder: (context, _) => const Icon(
+                  Icons.star,
+                  color: Colors.amber,
                 ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: RatingBar.builder(
-              initialRating: _rating,
-              minRating: 1,
-              direction: Axis.horizontal,
-              allowHalfRating: true,
-              itemCount: 5,
-              itemBuilder: (context, _) => const Icon(
-                Icons.star,
-                color: Colors.amber,
+                onRatingUpdate: (rating) {
+                  setState(() {
+                    _rating = rating; // Atualizar o rating
+                  });
+                },
               ),
-              onRatingUpdate: (rating) {
-                setState(() {
-                  _rating = rating; // Atualizar o rating
-                });
-              },
             ),
-          ),
-          const SizedBox(height: 16.0),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: TextField(
-              maxLines: 4,
-              controller: _comment,
-              decoration: InputDecoration(
-                hintText: 'Conte como foi sua experiência neste lugar',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8.0),
+            const SizedBox(height: 16.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: TextField(
+                maxLines: 4,
+                controller: _comment,
+                decoration: InputDecoration(
+                  hintText: 'Conte como foi sua experiência neste lugar',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
                 ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: TextButton.icon(
-              onPressed: () {},
-              icon: const Icon(Icons.add_a_photo, color: AppColors.lightPurple),
-              label: const Text(
-                'Adicionar foto',
-                style: TextStyle(color: AppColors.lightPurple),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: TextButton.icon(
+                onPressed: () {},
+                icon:
+                    const Icon(Icons.add_a_photo, color: AppColors.lightPurple),
+                label: const Text(
+                  'Adicionar foto',
+                  style: TextStyle(color: AppColors.lightPurple),
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                addCommentAndRating();
-              },
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(double.infinity, 48),
-                backgroundColor: AppColors.lightPurple,
-              ),
-              child: const Text(
-                'Enviar avaliação',
-                style: TextStyle(color: AppColors.white, fontSize: 18),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  addCommentAndRating();
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(double.infinity, 48),
+                  backgroundColor: AppColors.lightPurple,
+                ),
+                child: const Text(
+                  'Enviar avaliação',
+                  style: TextStyle(color: AppColors.white, fontSize: 18),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
